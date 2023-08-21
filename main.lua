ddc9fe90febba5d0e5f9a97651a75fedeb63050a
Mr.PH <111549574+sadraphh@users.noreply.github.com>
2023-08-21 05:27:58 +0330

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,16 @@
+-- Manifest data
+fx_version 'bodacious'
+games {'gta5'}
+
+-- Resource stuff
+name 'SP Anti DDoS'
+description 'Protect servers from Distributed Denial of Service "DDoS" attacks.'
+version 'v1.0'
+author 'Mr.PH'
+url 'https://github.com/sadraphh'
+
+-- Do not change anything,
+-- If you change the information, the service will be stopped .
+files {
+    'SP.cfg',
+}
